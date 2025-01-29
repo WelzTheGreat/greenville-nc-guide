102b785db668e69075635e3079544ce38580199a
WelzTheGreat <blessedman9023@gmail.com>
2025-01-29 00:44:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    <h1>Greenville, NC City Guide</h1>
+    <p>Welcome to the city guide!</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
